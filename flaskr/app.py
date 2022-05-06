1c399946be0eba0c9c071a5ac2c3aa040fe9b665
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-05-06 05:21:18 +0000

    init commit

---
+++ flaskr/app.py
@@ -0,0 +1,7 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,template_folder="./src/templates",static_folder="public")
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
